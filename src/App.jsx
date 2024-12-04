53f017b65fbf8288e3dd737db0315a6af29dbdaa
“tharindu <atdeshan@gmail.com>
2024-12-04 15:26:42 +0530

    six

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
       <Header />
 
       <Routes>
-        <Route path="tharindudeshan/" element={<Home />} />
-        <Route path="tharindudeshan/about" element={<About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         
       </Routes>
 
