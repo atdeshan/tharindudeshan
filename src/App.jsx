f8315fbd5a5a84a45f705a2329efce0ea7e78742
“tharindu <atdeshan@gmail.com>
2024-12-04 14:32:40 +0530

    six

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,24 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import About from "./components/about";
 import Home from "./components/home";
 import Header from "./components/header";
 import Fotter from "./components/footer";
 import "./styles.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <Header />
-        <Home />
-        <Fotter />
-      </>
-    ),
-  },
-  {
-    path: "/about",
-    element: (
-      <>
-        <Header />
-        <About />
-        <Fotter />
-      </>
-    ),
-  },
-], {
-  future: {
-    v7_startTransition: true,
-    v7_relativeSplatPath: true,
-  },
-});
-
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        
+      </Routes>
+
+      <Fotter />
+    </Router>
+    
+  );
 }
